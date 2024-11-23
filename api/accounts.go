781b77edcb891eae61f36ad00966177d5be8498a
askanymark <mark.paskanny@gmail.com>
2024-11-23 18:14:39 +0000

    feat(api): update account endpoint

--- api/accounts.go
+++ api/accounts.go
@@ -90,6 +90,53 @@ func (server *Server) listAccounts(context *gin.Context) {
 	context.JSON(http.StatusOK, accounts)
 }
 
+type updateAccountRequestParams struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+type updateAccountRequestBody struct {
+	Balance int64 `json:"balance" binding:"required"`
+}
+
+func (server *Server) updateAccount(ctx *gin.Context) {
+	var params updateAccountRequestParams
+	var body updateAccountRequestBody
+
+	if err := ctx.ShouldBindUri(&params); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	if err := ctx.ShouldBindJSON(&body); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(ctx, params.ID)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	account, err = server.store.UpdateAccount(ctx, db.UpdateAccountParams{
+		ID:      params.ID,
+		Balance: account.Balance + body.Balance,
+	})
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusNoContent, account)
+}
+
 type deleteAccountRequest struct {
 	ID int64 `uri:"id" binding:"required,min=1"`
 }
