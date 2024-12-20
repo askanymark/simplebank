2efd4d8dadbe08595b714a3abbb995116d46e5af
askanymark <mark.paskanny@gmail.com>
2024-12-20 16:05:46 +0200

    fix: wrong db uri env var

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "run db migrations"
 source /app/app.env
-/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+/app/migrate -path /app/migration -database "$DB_URI" -verbose up
 
 echo "start the app"
 exec "$@"
