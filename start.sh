71934cf9d09394f27d81b7dc120e130ba04efbbd
askanymark <mark.paskanny@gmail.com>
2024-12-20 16:47:09 +0200

    Revert "chore: do not run migrations"

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-#echo "run db migrations"
-#source /app/app.env
-#/app/migrate -path /app/migration -database "$DB_URI" -verbose up
+echo "run db migrations"
+source /app/app.env
+/app/migrate -path /app/migration -database "$DB_URI" -verbose up
 
 echo "start the app"
 exec "$@"
