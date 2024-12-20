6a9539b40a417aaeb210013dcb3ffcf8504c4454
askanymark <mark.paskanny@gmail.com>
2024-12-20 16:42:05 +0200

    chore: do not run migrations

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 set -e
 
-echo "run db migrations"
-source /app/app.env
-cat /app/app.env
-echo $DB_URI
-/app/migrate -path /app/migration -database "$DB_URI" -verbose up
+#echo "run db migrations"
+#source /app/app.env
+#/app/migrate -path /app/migration -database "$DB_URI" -verbose up
 
 echo "start the app"
 exec "$@"
