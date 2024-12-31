def9dc78b8c37d39d058dcf8910762bfbb6396a2
askanymark <mark.paskanny@gmail.com>
2024-12-31 11:54:05 +0200

    feat: implement login user via grpc

---
+++ gapi/rpc_login_user.go
@@ -0,0 +1,62 @@
+package gapi
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	db "simplebank/db/sqlc"
+	"simplebank/pb"
+	"simplebank/util"
+)
+
+func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	user, err := server.store.GetUser(ctx, req.GetUsername())
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, status.Errorf(codes.NotFound, "cannot find user: %v", err)
+		}
+
+		return nil, status.Errorf(codes.Internal, "cannot get user: %v", err)
+	}
+
+	err = util.CheckPassword(req.GetPassword(), user.HashedPassword)
+	if err != nil {
+		return nil, status.Errorf(codes.Unauthenticated, "unauthenticated")
+	}
+
+	accessToken, accessPayload, err := server.tokenMaker.CreateToken(user.Username, server.config.AccessTokenDuration)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "cannot create access token: %v", err)
+	}
+
+	refreshToken, refreshPayload, err := server.tokenMaker.CreateToken(user.Username, server.config.RefreshTokenDuration)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "cannot create refresh token: %v", err)
+	}
+
+	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
+		ID:           refreshPayload.ID,
+		Username:     user.Username,
+		RefreshToken: refreshToken,
+		UserAgent:    "",
+		ClientIp:     "",
+		IsBlocked:    false,
+		ExpiresAt:    refreshPayload.ExpiredAt,
+	})
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "cannot create session: %v", err)
+	}
+
+	response := &pb.LoginUserResponse{
+		SessionId:             session.ID.String(),
+		AccessToken:           accessToken,
+		AccessTokenExpiresAt:  timestamppb.New(accessPayload.ExpiredAt),
+		RefreshToken:          refreshToken,
+		RefreshTokenExpiresAt: timestamppb.New(refreshPayload.ExpiredAt),
+		User:                  convertUser(user),
+	}
+	return response, nil
+}
