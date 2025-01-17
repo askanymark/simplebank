e2b2eeb6d272be1749ac38221c161d2a6d8bf22b
askanymark <mark.paskanny@gmail.com>
2025-01-17 17:07:28 +0000

    feat: worker logging

--- worker/processor.go
+++ worker/processor.go
@@ -3,6 +3,7 @@ package worker
 import (
 	"context"
 	"github.com/hibiken/asynq"
+	"github.com/rs/zerolog/log"
 	db "simplebank/db/sqlc"
 )
 
@@ -27,6 +28,14 @@ func NewRedisTaskProcessor(options asynq.RedisClientOpt, store db.Store) TaskPro
 			QueueCritical: 10,
 			QueueDefault:  5,
 		},
+		ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+			log.Error().
+				Err(err).
+				Str("type", task.Type()).
+				Bytes("payload", task.Payload()).
+				Msg("failed to process task")
+		}),
+		Logger: NewLogger(),
 	})
 
 	return &RedisTaskProcessor{
