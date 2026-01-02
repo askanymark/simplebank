62218eec0a8be8f4fe09d10661c21763390e21db
askanymark <mark.paskanny@gmail.com>
2026-01-02 13:03:06 +0200

    feat(accounts): implementation

---
+++ gapi/rpc_list_accounts.go
@@ -0,0 +1,69 @@
+package gapi
+
+import (
+	"context"
+	db "simplebank/db/sqlc"
+	"simplebank/pb"
+	"simplebank/util"
+	"simplebank/val"
+
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *Server) ListAccounts(ctx context.Context, req *pb.ListAccountsRequest) (*pb.ListAccountsResponse, error) {
+	authPayload, err := server.authorizeUser(ctx, []string{util.DepositorRole, util.BankerRole})
+	if err != nil {
+		return nil, unauthenticatedError(err)
+	}
+
+	violations := validateListAccountsRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
+	// Only bankers can list accounts of other users
+	if req.Username != nil && !util.IsBanker(authPayload.Role) {
+		return nil, status.Errorf(codes.PermissionDenied, "cannot list accounts of other users")
+	}
+
+	username := authPayload.Username
+	if req.Username != nil {
+		username = req.GetUsername()
+	}
+
+	// Find accounts the user owns
+	accounts, err := server.store.ListAccounts(ctx, db.ListAccountsParams{
+		Owner:  username,
+		Limit:  10,
+		Offset: 0,
+	})
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to list accounts: %v", err)
+	}
+
+	response := &pb.ListAccountsResponse{
+		Pagination: &pb.Pagination{
+			// TODO cursor
+			Count: int64(len(accounts)),
+		},
+		Data: make([]*pb.Account, len(accounts)),
+	}
+
+	for i, account := range accounts {
+		response.Data[i] = account.ToResponse()
+	}
+
+	return response, nil
+}
+
+func validateListAccountsRequest(req *pb.ListAccountsRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if req.Username != nil {
+		if err := val.ValidateUsername(req.GetUsername()); err != nil {
+			violations = append(violations, fieldViolation("username", err))
+		}
+	}
+
+	return violations
+}
---
+++ gapi/rpc_list_accounts_test.go
@@ -0,0 +1,189 @@
+package gapi
+
+import (
+	"context"
+	mockdb "simplebank/db/mock"
+	db "simplebank/db/sqlc"
+	"simplebank/pb"
+	"simplebank/token"
+	"simplebank/util"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func TestListAccounts(t *testing.T) {
+	user, _ := randomUser(t)
+	banker, _ := randomUser(t)
+	banker.Role = util.BankerRole
+	otherUser, _ := randomUser(t)
+
+	n := 5
+	accounts := make([]db.Account, n)
+	for i := 0; i < n; i++ {
+		accounts[i] = randomAccount(user.Username)
+	}
+
+	otherAccounts := make([]db.Account, n)
+	for i := 0; i < n; i++ {
+		otherAccounts[i] = randomAccount(otherUser.Username)
+	}
+
+	testCases := []struct {
+		name          string
+		req           *pb.ListAccountsRequest
+		buildStubs    func(store *mockdb.MockStore)
+		buildContext  func(t *testing.T, tokenMaker token.Maker) context.Context
+		checkResponse func(t *testing.T, res *pb.ListAccountsResponse, err error)
+	}{
+		{
+			"OK",
+			&pb.ListAccountsRequest{
+				Limit: 10,
+			},
+			func(store *mockdb.MockStore) {
+				arg := db.ListAccountsParams{
+					Owner:  user.Username,
+					Limit:  10,
+					Offset: 0,
+				}
+
+				store.EXPECT().
+					ListAccounts(gomock.Any(), gomock.Eq(arg)).
+					Times(1).
+					Return(accounts, nil)
+			},
+			func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return newContextWithBearerToken(t, tokenMaker, user, time.Minute)
+			},
+			func(t *testing.T, res *pb.ListAccountsResponse, err error) {
+				require.NoError(t, err)
+				require.NotNil(t, res)
+				require.Len(t, res.Data, n)
+			},
+		},
+		{
+			"OKBanker",
+			&pb.ListAccountsRequest{
+				Username: &otherUser.Username,
+				Limit:    10,
+			},
+			func(store *mockdb.MockStore) {
+				arg := db.ListAccountsParams{
+					Owner:  otherUser.Username,
+					Limit:  10,
+					Offset: 0,
+				}
+
+				store.EXPECT().
+					ListAccounts(gomock.Any(), gomock.Eq(arg)).
+					Times(1).
+					Return(otherAccounts, nil)
+			},
+			func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return newContextWithBearerToken(t, tokenMaker, banker, time.Minute)
+			},
+			func(t *testing.T, res *pb.ListAccountsResponse, err error) {
+				require.NoError(t, err)
+				require.NotNil(t, res)
+				require.Len(t, res.Data, n)
+			},
+		},
+		{
+			"PermissionDenied",
+			&pb.ListAccountsRequest{
+				Username: &otherUser.Username,
+			},
+			func(store *mockdb.MockStore) {
+				store.EXPECT().
+					ListAccounts(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return newContextWithBearerToken(t, tokenMaker, user, time.Minute)
+			},
+			func(t *testing.T, res *pb.ListAccountsResponse, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.PermissionDenied, st.Code())
+			},
+		},
+		{
+			"Unauthorized",
+			&pb.ListAccountsRequest{},
+			func(store *mockdb.MockStore) {
+				store.EXPECT().
+					ListAccounts(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return context.Background()
+			},
+			func(t *testing.T, res *pb.ListAccountsResponse, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.Unauthenticated, st.Code())
+			},
+		},
+		{
+			"InternalError",
+			&pb.ListAccountsRequest{},
+			func(store *mockdb.MockStore) {
+				store.EXPECT().
+					ListAccounts(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return([]db.Account{}, context.DeadlineExceeded)
+			},
+			func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return newContextWithBearerToken(t, tokenMaker, user, time.Minute)
+			},
+			func(t *testing.T, res *pb.ListAccountsResponse, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.Internal, st.Code())
+			},
+		},
+		{
+			"InvalidUsername",
+			&pb.ListAccountsRequest{
+				Username: func() *string { s := "invalid#user"; return &s }(),
+			},
+			func(store *mockdb.MockStore) {
+				store.EXPECT().
+					ListAccounts(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return newContextWithBearerToken(t, tokenMaker, banker, time.Minute)
+			},
+			func(t *testing.T, res *pb.ListAccountsResponse, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.InvalidArgument, st.Code())
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			storeCtrl := gomock.NewController(t)
+			defer storeCtrl.Finish()
+			store := mockdb.NewMockStore(storeCtrl)
+
+			tc.buildStubs(store)
+
+			server := newTestServer(t, store, nil)
+			ctx := tc.buildContext(t, server.tokenMaker)
+			res, err := server.ListAccounts(ctx, tc.req)
+			tc.checkResponse(t, res, err)
+		})
+	}
+}
