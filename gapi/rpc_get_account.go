b30678f054938e0e84fdf7194f699ca3a7ed077a
askanymark <mark.paskanny@gmail.com>
2026-01-02 13:24:57 +0200

    feat(accounts): implementation

---
+++ gapi/rpc_get_account.go
@@ -0,0 +1,34 @@
+package gapi
+
+import (
+	"context"
+	"errors"
+	db "simplebank/db/sqlc"
+	"simplebank/pb"
+	"simplebank/util"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *Server) GetAccount(ctx context.Context, req *pb.GetAccountRequest) (*pb.Account, error) {
+	authPayload, err := server.authorizeUser(ctx, []string{util.DepositorRole, util.BankerRole})
+	if err != nil {
+		return nil, unauthenticatedError(err)
+	}
+
+	account, err := server.store.GetAccount(ctx, req.GetAccountId())
+	if err != nil {
+		if errors.Is(err, db.ErrRecordNotFound) {
+			return nil, status.Errorf(codes.NotFound, "account not found")
+		}
+
+		return nil, status.Errorf(codes.Internal, "failed to fetch account details")
+	}
+
+	if account.Owner != authPayload.Username {
+		return nil, status.Errorf(codes.PermissionDenied, "invalid account owner")
+	}
+
+	return account.ToResponse(), nil
+}
---
+++ gapi/rpc_get_account_test.go
@@ -0,0 +1,152 @@
+package gapi
+
+import (
+	"context"
+	mockdb "simplebank/db/mock"
+	db "simplebank/db/sqlc"
+	"simplebank/pb"
+	"simplebank/token"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func TestGetAccount(t *testing.T) {
+	user, _ := randomUser(t)
+	account := randomAccount(user.Username)
+
+	testCases := []struct {
+		name          string
+		req           *pb.GetAccountRequest
+		buildStubs    func(store *mockdb.MockStore)
+		buildContext  func(t *testing.T, tokenMaker token.Maker) context.Context
+		checkResponse func(t *testing.T, res *pb.Account, err error)
+	}{
+		{
+			"OK",
+			&pb.GetAccountRequest{
+				AccountId: account.ID,
+			},
+			func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(1).
+					Return(account, nil)
+			},
+			func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return newContextWithBearerToken(t, tokenMaker, user, time.Minute)
+			},
+			func(t *testing.T, res *pb.Account, err error) {
+				require.NoError(t, err)
+				require.NotNil(t, res)
+				require.Equal(t, account.ID, res.Id)
+				require.Equal(t, account.Owner, res.Owner)
+			},
+		},
+		{
+			"NotFound",
+			&pb.GetAccountRequest{
+				AccountId: account.ID,
+			},
+			func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(1).
+					Return(db.Account{}, db.ErrRecordNotFound)
+			},
+			func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return newContextWithBearerToken(t, tokenMaker, user, time.Minute)
+			},
+			func(t *testing.T, res *pb.Account, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.NotFound, st.Code())
+			},
+		},
+		{
+			"InternalError",
+			&pb.GetAccountRequest{
+				AccountId: account.ID,
+			},
+			func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(1).
+					Return(db.Account{}, context.DeadlineExceeded)
+			},
+			func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return newContextWithBearerToken(t, tokenMaker, user, time.Minute)
+			},
+			func(t *testing.T, res *pb.Account, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.Internal, st.Code())
+			},
+		},
+		{
+			"PermissionDenied",
+			&pb.GetAccountRequest{
+				AccountId: account.ID,
+			},
+			func(store *mockdb.MockStore) {
+				otherUser, _ := randomUser(t)
+				otherAccount := randomAccount(otherUser.Username)
+
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(1).
+					Return(otherAccount, nil)
+			},
+			func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return newContextWithBearerToken(t, tokenMaker, user, time.Minute)
+			},
+			func(t *testing.T, res *pb.Account, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.PermissionDenied, st.Code())
+			},
+		},
+		{
+			"Unauthorized",
+			&pb.GetAccountRequest{
+				AccountId: account.ID,
+			},
+			func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return context.Background()
+			},
+			func(t *testing.T, res *pb.Account, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.Unauthenticated, st.Code())
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			storeCtrl := gomock.NewController(t)
+			defer storeCtrl.Finish()
+			store := mockdb.NewMockStore(storeCtrl)
+
+			tc.buildStubs(store)
+
+			server := newTestServer(t, store, nil)
+			ctx := tc.buildContext(t, server.tokenMaker)
+			res, err := server.GetAccount(ctx, tc.req)
+			tc.checkResponse(t, res, err)
+		})
+	}
+}
